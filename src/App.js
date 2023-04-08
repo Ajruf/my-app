365da98380f61bad9b6cb5bdedc471be92074f0f
Ajruf <ajruf@comcast.net>
2023-04-07 21:17:54 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useState } from 'react';
+import { supabase } from './supabaseClient';
 import logo from './logo.svg';
 import './App.css';
-
+function Mbutton() {
+  const [count, setCount] = useState(0);
+  function doMagic () {
+    setCount(count + 1);
+  }
+  return (
+    <>
+    <h2>The Magic Button</h2>
+    <button onClick={doMagic}>M {count}</button>
+    </>
+  )
+}
 function App() {
   return (
     <div className="App">
@@ -17,9 +30,40 @@ function App() {
         >
           Learn React with Andrew and MSU
         </a>
+        <Mbutton />
       </header>
     </div>
   );
+}
+
+function Library() {
+  // The useState hook lets us store data in a component across renders
+  // setMyBooks is a setter function that updates the state of myBooks
+  const [myBooks, setMyBooks] = useState([]);
+  // This should look familar from Codepen
+  async function getBooks() {
+    let { data: Books, error } = await supabase
+      .from('Books')
+      .select('*')
+    // Update the state
+    setMyBooks(Books);
+  }
+  // Execute the function
+  getBooks();
+  // Below is what displays when you use <Library />
+  return (
+    <table>
+    {
+      myBooks.map(b => (
+        <tr>
+          <td>{b.title}</td>
+          <td>{b.author}</td>
+          <td>{b.isbn}</td>
+        </tr>
+      ))
+    }
+    </table>
+  )
 }
 
 export default App;
---
+++ src/supabaseClient.js
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js"
+
+const supabaseUrl = 'https://ngojdtfmnxqgixjxdykv.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5nb2pkdGZtbnhxZ2l4anhkeWt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzkzMzAzNzIsImV4cCI6MTk5NDkwNjM3Mn0.KaBRg47s6uwwYE8xFaff_8IY6y3EL40__9mQk-J9wVo'
+const supabase = createClient(supabaseUrl, supabaseKey)
